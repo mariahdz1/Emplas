b17c7c7be8189abf7601a91a35b683a158e3d6af
jessicart <inverjessie@gmail.com>
2022-08-15 09:56:09 -0500

    Clases con estructura básica sin métodos

---
+++ Sprint2/src/main/java/com/mycompany/sprint2/Enum_RoleName.java
@@ -0,0 +1,6 @@
+package com.mycompany.sprint2;
+
+
+public enum Enum_RoleName {
+    Admin,Operario
+}
---
+++ Sprint2/src/main/java/com/mycompany/sprint2/Main.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.sprint2;
+
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {              
+      /*Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese el nombre de la empresa");*/
+    }
+    
+    public void CrearEmpresa(long id, String name, String document, String phone, String address, User[] users, Transaction[] transactions, Date createdAt){
+      Enterprise Empresa =new Enterprise (id,name,document,phone,address, users, transactions,createdAt);
+            
+    }
+}
