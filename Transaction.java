6817b035530cc475e26d9f880f51b69fa688483b
imbercarmina <110949680+imbercarmina@users.noreply.github.com>
2022-08-24 21:41:14 -0500

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,67 @@
+
+package com.mycompany.sprint2;
+
+import java.util.Date;
+
+public class Transaction {
+    private long id;
+    private String concept;
+    private float amount;
+    private Enterprise enterprise;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Transaction(long id, String concept, float amount, Enterprise enterprise, Date createdAt) {
+        this.id = id;
+        this.concept = concept;
+        this.amount = amount;
+        this.enterprise = enterprise;
+        this.createdAt = createdAt;
+        
+        
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+    
+    
+    
+}
