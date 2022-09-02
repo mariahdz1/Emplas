05b083f725f64781ebd8c7524197ae97b64c71a1
jessicart <inverjessie@gmail.com>
2022-09-02 09:53:36 -0500

    Proyecto Pentagono avances 2 Septiembre 9.53am

--- pentagono/pentagono/src/main/java/com/pentagono/pentagono/repository/IEnum_RoleName.java
+++
@@ -1,8 +0,0 @@
-package com.pentagono.pentagono.repository;/*jessica 1sept*/
-
-import com.pentagono.pentagono.model.Enterprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface IEnum_RoleName extends IGenericRepository<Enterprise,Long> {
-}
--- pentagono/pentagono/src/main/java/com/pentagono/pentagono/repository/ITransaction.java
+++
@@ -1,8 +0,0 @@
-package com.pentagono.pentagono.repository;/*Jessica 1 sept*/
-
-import com.pentagono.pentagono.model.Enterprise;
-
-
-
-public interface ITransaction extends IGenericRepository<Enterprise,Long> {
-}
